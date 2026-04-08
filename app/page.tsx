69c4c2eed313d287aab504e80b74eebf7b0ec1b2
Dexter-in <arjunarjun.k88@gmail.com>
2026-04-08 22:00:58 +0530

    again and again

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
-import { events } from "@/lib/constants";
+
 import { cacheLife } from "next/cache";
 
 
